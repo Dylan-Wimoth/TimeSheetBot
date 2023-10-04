08e1668b263c3177a0902083166fd9805dca7c03
nuklhd823 <nuklhd823@hotmail.com>
2023-10-04 11:32:06 -0400

    tokenVerifyChanges: got rid of repeated code and created an automated way to refresh the token for the TimeSheetBot

--- calendar_integration.py
+++ calendar_integration.py
@@ -6,6 +6,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import os
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
@@ -31,11 +32,11 @@ def generateToken():
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            #Delete the token an make a new one
+            os.remove('token.json')
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
