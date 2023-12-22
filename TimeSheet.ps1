1d0d794a9e082f3bc115f6feb73abf5b68badcaf
Cary Gerhardt <130471606+nuklhd823@users.noreply.github.com>
2023-12-22 16:35:07 -0500

    Updates powershell script with try catch blocks

--- TimeSheet.ps1
+++ TimeSheet.ps1
@@ -3,11 +3,34 @@ Set-Location -Path $filepath
 try{
     #Try to use python
     python config.py
+}
+Catch{
+    #Else use python3
+    Write-Error "An Error calling python config.py"
+}
+try{
+    #Try to use python
+    python3 config.py
+}
+Catch{
+    #Else use python3
+    Write-Error "An Error calling python3 config.py"
+}
+try{
+    #Try to use python
     python main.py
 }
 Catch{
     #Else use python3
-    python3 config.py
-    python3 main.py
+    Write-Error "An Error calling python main.py"
 }
+try{
+    #Try to use python
+    python3 config.py
+}
+Catch{
+    #Else use python3
+    Write-Error "An Error calling python3 main.py"
+}
+
     
